c6b92067551de7214d1274f0a7c519db95a6643f
Diego22rct <diegorafaelcisnerostafur@gmail.com>
2024-04-25 19:01:18 -0500

    feat(not-found-path): functionality was added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// Import our components
+import { PageNotFoundComponent } from './public/pages/page-not-found/page-not-found.component';
+
+const routes: Routes = [
+  { path: '**', component: PageNotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
