83cd58566c1675be4ed83ba7398a98e1dab0baf6
Diego22rct <diegorafaelcisnerostafur@gmail.com>
2024-04-26 21:33:42 -0500

    feat(group): added group service

---
+++ src/app/group/services/group.service.ts
@@ -0,0 +1,15 @@
+import { GroupEntity } from '../model/group.entity';
+import { BaseService } from './../../shared/services/base.service';
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GroupService extends BaseService<GroupEntity> {
+
+  constructor(http: HttpClient) {
+    super(http);
+    this.resourceEndpoint = '/groups';
+  }
+}
