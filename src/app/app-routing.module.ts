3a785223c10951f3dc0b3fea0823b81b787a9624
Diego22rct <diegorafaelcisnerostafur@gmail.com>
2024-04-26 21:43:48 -0500

    feat(group): Added PageGroup component to display all groups

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { RouterModule, Routes } from '@angular/router';
 // Import our components
 import { PageNotFoundComponent } from './public/pages/page-not-found/page-not-found.component';
 import { PageCreateGroupComponent } from './group/pages/page-create-group/page-create-group.component';
-import {HomeComponent} from "./public/pages/home/home.component";
+import { HomeComponent } from "./public/pages/home/home.component";
+import { PageGroupComponent } from "./group/pages/page-group/page-group.component";
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'groups', component: PageGroupComponent },
   { path: 'create-group', component: PageCreateGroupComponent },
   { path: '**', component: PageNotFoundComponent },
 ];
