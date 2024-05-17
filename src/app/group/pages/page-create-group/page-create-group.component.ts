4b25982cc649dd5e4bbfc726de1c2d744b656246
Diego22rct <diegorafaelcisnerostafur@gmail.com>
2024-05-17 13:48:12 -0500

    feat: Add redirect to group list after creating a new group

--- src/app/group/pages/page-create-group/page-create-group.component.ts
+++ src/app/group/pages/page-create-group/page-create-group.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { GroupService } from '../../services/group.service';
 import { GroupEntity } from '../../model/group.entity';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-page-create-group',
@@ -8,12 +9,18 @@ import { GroupEntity } from '../../model/group.entity';
   styleUrl: './page-create-group.component.css'
 })
 export class PageCreateGroupComponent {
-  constructor(private groupService: GroupService) { }
+  constructor(private groupService: GroupService, private router: Router) { }
 
   createNewGroup(group: GroupEntity) {
     console.log('Creating new group... In parent component');
     this.groupService.create(group).subscribe((response) => {
       console.log('Group created successfully', response);
+      this.redirectToGroupList();
     });
+  }
+
+  redirectToGroupList() {
+    console.log('Redirecting to group list...');
+    this.router.navigate(['/groups']);
   }
 }
