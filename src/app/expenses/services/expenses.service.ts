7450c9cfc81e46da0d7066bcff8175e9177ac838
Fiorella <U202120418@upc.edu.pe>
2024-06-04 16:37:57 -0500

    update(home): welcome card

--- src/app/expenses/services/expenses.service.ts
+++ src/app/expenses/services/expenses.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { BaseService } from '../../shared/services/base.service';
 import { ExpensesEntity } from '../model/expenses.entity';
 import { HttpClient } from "@angular/common/http";
+import {Observable} from "rxjs";
+import {PartnerEntity} from "../../pockets/model/partnerEntity";
 
 @Injectable({
   providedIn: 'root'
@@ -12,4 +14,11 @@ export class ExpensesService extends BaseService<ExpensesEntity> {
     super(http);
     this.resourceEndpoint = '/expenses';
   }
+
+  getExpensesByUserId(userId: number): Observable<ExpensesEntity[]> {
+    return this.http.get<ExpensesEntity[]>(`${this.resourcePath()}/${userId}`, this.httpOptions);
+  }
+
 }
+
+
