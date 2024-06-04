a1faa868907652864f9bb9267a455a0734990773
Fiorella <U202120418@upc.edu.pe>
2024-06-04 16:13:54 -0500

    update(home): backend implementation pt1

--- src/app/pockets/model/partnerEntity.ts
+++ src/app/pockets/model/partnerEntity.ts
@@ -1,41 +1,17 @@
 export class PartnerEntity {
   id: number;
-  name: string;
+  fullName: string;
   email: string;
+  phoneNumber: string;
   password: string;
-  balance: number;
   photo: string;
-  contacts: [
-    {
-      id: number;
-      name: string;
-    }
-  ];
-  groups: [
-    {
-      id: number;
-      name: string;
-    }
-  ];
 
   constructor() {
     this.id = 0;
-    this.name = "";
+    this.fullName = "";
     this.email = "";
+    this.phoneNumber = "";
     this.password = "";
-    this.balance = 0;
-    this.contacts = [
-      {
-        id: 0,
-        name: "",
-      }
-    ];
-    this.groups = [
-      {
-        id: 0,
-        name: "",
-      }
-    ];
     this.photo = "";
   }
 
--- src/app/pockets/pages/home/home.component.ts
+++ src/app/pockets/pages/home/home.component.ts
@@ -1,10 +1,33 @@
-import {Component} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {PartnerEntity} from "../../model/partnerEntity";
+import {PartnerService} from "../../services/Partner.service";
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  partner: PartnerEntity | undefined;
+  userId: number = 1; // Aquí defines el ID del usuario que quieres obtener
 
+  constructor(private partnerService: PartnerService) { }
+
+  ngOnInit(): void {
+    this.getPartnerById(this.userId);
+  }
+
+  getPartnerById(id: number): void {
+    this.partnerService.getPartnerById(id)
+      .subscribe(
+        (partner: PartnerEntity) => {
+          this.partner = partner;
+          console.log('Usuario obtenido:', this.partner);
+        },
+        (error) => {
+          console.error('Error al obtener usuario por ID:', error);
+        }
+      );
+  }
 }
