1570bd6059c7175df223179dc022b0a4fa4648ed
Diego22rct <diegorafaelcisnerostafur@gmail.com>
2024-06-21 23:30:41 -0500

    fix(add expense): fix the creation flow in add expenses now can add a expense normally
    expense.component.html` file update the `mat-option` element to bind the `group.id` property instead of `group.groupId`. This change ensures that the correct group ID is selected when picking a group in the expense form.

--- src/app/expenses/pages/add-expense/add-expense.component.ts
+++ src/app/expenses/pages/add-expense/add-expense.component.ts
@@ -4,6 +4,7 @@ import { PartnerService } from '../../../pockets/services/Partner.service';
 import { ExpensesEntity } from '../../model/expenses.entity';
 import { ExpensesService } from '../../services/expenses.service';
 import { AuthenticationService } from '../../../iam/services/authentication.service';
+import { GroupService } from '../../../group/services/group.service';
 
 @Component({
   selector: 'app-add-expense',
@@ -11,17 +12,26 @@ import { AuthenticationService } from '../../../iam/services/authentication.serv
   styleUrl: './add-expense.component.css'
 })
 export class AddExpenseComponent implements OnInit {
-  private userId: any;
-  joinedGroups: any;
-  constructor(private partnerService: PartnerService, private expenseService: ExpensesService, private authenticationService: AuthenticationService) { }
+  userId: number = 0;
+  joinedGroups: any = [];
+  constructor(
+    private partnerService: PartnerService,
+    private expenseService: ExpensesService,
+    private authenticationService: AuthenticationService,
+    private groupService: GroupService,
+  ) { }
   user: PartnerEntity = new PartnerEntity();
+
   ngOnInit(): void {
-    this.userId = this.authenticationService.currentUserId.subscribe((userId: any) => {
+    this.authenticationService.currentUserId.subscribe((userId: any) => {
       this.userId = userId;
-      this.partnerService.getPartnerById(this.userId).subscribe((partner: any) => {
-        this.user = partner;
-        this.expenseService.getJoinedUserGroups(this.user.id).subscribe((groups: any) => {
-          this.joinedGroups = groups;
+      this.partnerService.getPartnerById(userId).subscribe((partner: any) => {
+        this.expenseService.getJoinedUserGroups(userId).subscribe((groups: any) => {
+          groups.forEach((group: any) => {
+            this.groupService.getById(group.groupId).subscribe((group: any) => {
+              this.joinedGroups.push(group);
+            });
+          });
         });
       });
     });
@@ -30,8 +40,9 @@ export class AddExpenseComponent implements OnInit {
   onSubmit(expense: ExpensesEntity) {
     // delete un necessary properties
     let expenseClean: any = { ...expense };
+    expenseClean.userId = this.userId;
     delete expenseClean.createdAt;
     delete expenseClean.updatedAt;
-    this.expenseService.create(expense).subscribe();
+    this.expenseService.create(expenseClean).subscribe();
   }
 }
