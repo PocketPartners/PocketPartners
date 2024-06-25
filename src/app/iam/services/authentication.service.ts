25d048c0f011d4a9a0a1a72d5c31051ebfc5e77f
Diego22rct <diegorafaelcisnerostafur@gmail.com>
2024-06-24 20:16:43 -0500

    feat(aim): validate if is production mode to log the requests or not

--- src/app/iam/services/authentication.service.ts
+++ src/app/iam/services/authentication.service.ts
@@ -43,7 +43,7 @@ export class AuthenticationService {
     return this.http.post<SignUpResponse>(`${this.basePath}/authentication/sign-up`, signUpRequest, this.httpOptions)
       .subscribe({
         next: (response) => {
-          console.log(`Signed up as ${response.username} with id: ${response.id}`);
+          // console.log(`Signed up as ${response.username} with id: ${response.id}`);
           const infoToSave = new SignInInfo(signInInfo.firstName, signInInfo.lastName, signInInfo.phoneNumber, signInInfo.photo, signInInfo.email, response.id);
           this.currentUserInformation = new BehaviorSubject<SignInInfo>(infoToSave);
           this.router.navigate(['/sign-in']).then();
